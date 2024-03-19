bcae09ca7609423825b636715c132e13982932f2
Kieran Mueller (OSV) <kieran.mueller.osv@fedex.com>
2024-03-19 12:48:38 -0500

    keyboard highlighting

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -9,18 +9,20 @@ export class GameBoardComponent {
   currentGuessLength = 0;
   word = 'cradle';
   width = this.word.length;
-  height = 6;
-  charArr = Array(this.width * this.height).fill(0);
+  maxAttempts = 6;
+  attemptCount = 0;
+  charArr = Array(this.width * this.maxAttempts).fill(0);
   usedChars: Array<string> = [];
   currentRow = 0;
 
+  // Board is initialized, charArr is initialized to array of width x height size, filled with 0's.
+  // Conditionally not rendering 0s in html
+  // this type() method, finds first element that is 0, replaces with the typed character, 
+  // until current guess length === word.length
   type(value: any) {
-    for (let i = 0; i < this.charArr.length; i++) {
-      if (!this.charArr[i] && this.currentGuessLength < this.word.length) {
-        this.charArr[i] = value;
-        this.currentGuessLength++;
-        break;
-      }
+    if (this.currentGuessLength < this.word.length) {
+      this.charArr[this.charArr.indexOf(0)] = value;
+      this.currentGuessLength++;
     }
   }
 
@@ -33,10 +35,38 @@ export class GameBoardComponent {
     }
     console.log(guess);
     // handle valid word verification, else dont move on
+    this.checkGuess(guess)
+    //
+    this.attemptCount++;
     this.usedChars.push(...guess);
     console.log(this.usedChars);
     this.currentRow++;
   }
 
+  checkGuess(guess: string) {
+    if (guess === this.word) {
+      alert('Correct')
+      this.gameOver('win')
+      return;
+    }
+  }
+
+  handleCharHighlight() {
+
+  }
+
+  gameOver(status: string) {
+    if (status === 'win') {
+      alert('You Win')
+    }
+  }
+
   del() {}
+
+  checkChar(char: string, index: number) {
+    console.log(char + ": " + index)
+    return {
+      'right-spot': char == 'a'
+    }
+  }
 }
