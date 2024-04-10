5cb6ed7503d6ba9bd906176fa2562c62faf7d2a0
KieranMueller <kieran98mueller@gmail.com>
2024-04-09 20:19:16 -0500

    http to https url

--- environment-variables.ts
+++ environment-variables.ts
@@ -1,3 +1,3 @@
 export const localBaseUrl = 'http://localhost:8080';
 export const prodBaseUrl =
-  'http://wordle-kieran-api.us-east-2.elasticbeanstalk.com';
+  'https://wordle-kieran-api.us-east-2.elasticbeanstalk.com';
