32863a5f3dd66ae66f58eb70e86ddd45137ffe41
Kieran Mueller <kieran98mueller@gmail.com>
2024-04-12 18:11:50 -0500

    Update environment-variables.ts

--- environment-variables.ts
+++ environment-variables.ts
@@ -1,5 +1,5 @@
-export const backendBaseUrl = 'http://localhost:8080';
-export const frontendBaseUrl = 'http://localhost:4200';
+export const backendBaseUrl = 'https://wordle-kieran-api.us-east-2.elasticbeanstalk.com';
+export const frontendBaseUrl = 'https://wordle-by-kieran.netlify.app';
 
 // http://localhost:8080
 // http://localhost:4200
