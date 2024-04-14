b2a3c2b1722a7317e07c014cf499aa6f0ccce121
KieranMueller <kieran98mueller@gmail.com>
2024-04-14 17:38:40 -0500

    urls

--- environment-variables.ts
+++ environment-variables.ts
@@ -1,5 +1,5 @@
-export const backendBaseUrl = 'http://localhost:8080';
-export const frontendBaseUrl = 'http://localhost:4200';
+export const backendBaseUrl = 'http://wordle-kieran-api.us-east-2.elasticbeanstalk.com';
+export const frontendBaseUrl = 'https://wordle.kieranmueller.com';
 
 // http://localhost:8080
 // http://localhost:4200
