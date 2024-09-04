9ab131b7e5e57f3c3c7f462bd6b700f4feb6af49
KieranMueller <kieran98mueller@gmail.com>
2024-09-03 23:13:20 -0500

    new backend

--- environment-variables.ts
+++ environment-variables.ts
@@ -1,10 +1,11 @@
-export const backendBaseUrl = 'https://wordle-api.kmcodes.com';
+export const backendBaseUrl = 'https://wordle-backend-e6dwaxd9gfevc5c2.eastus2-01.azurewebsites.net';
 export const frontendBaseUrl = 'https://wordle.kmcodes.com';
 
 // http://localhost:8080
 // http://localhost:4200
 
 // new
+// https://wordle-backend-e6dwaxd9gfevc5c2.eastus2-01.azurewebsites.net
 // https://wordle-api.kmcodes.com
 // https://wordle.kmcodes.com
 
